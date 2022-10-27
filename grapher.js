96c58989065ffa69f1d306a7e1ef1d368f3194dc
Emmy43262 <73496800+Emmy43262@users.noreply.github.com>
2022-10-27 18:31:53 +0300

    Added zoom buttons and multiple functions support. Discontinous functions rendering still in progress

--- grapher.js
+++ grapher.js
@@ -1,13 +1,25 @@
 const g = document.getElementById("grapher");
 const c = g.getContext("2d");
+g.width = document.documentElement.clientWidth;
+g.height = document.documentElement.clientHeight;
+
 const width = g.width;
 const height = g.height;
 
 const canvasData = c.getImageData(0,0,width,height);
 
 const offset = {x:0,y:0};
-const zoom = 50; //pixels per unit
+let zoom = 50; //pixels per unit
 const precision = 1; //pixel gap between evaluations
+
+document.getElementById("magnify").addEventListener("click",()=>{
+    zoom *= 1.2;
+    Draw();
+});
+document.getElementById("demagnify").addEventListener("click",()=>{
+    zoom *= .83;
+    Draw();
+});
 
 g.addEventListener("mousedown",function(event){
     g.addEventListener("mousemove",ChangeOffset);
@@ -35,18 +47,18 @@ DrawAxes = (()=>{
     c.lineWidth = 2;
     c.strokeStyle = "#000";
 
-    if(Math.abs(offset.x/zoom) <= width/2)
+    if(Math.abs(offset.x) <= width/2)
     {
-        const _x = offset.x/zoom + width/2;
+        const _x = offset.x + width/2;
         c.beginPath();
         c.moveTo(_x,0);
         c.lineTo(_x,height);
         c.stroke();
     }
 
-    if(Math.abs(offset.y/zoom) <= height/2)
+    if(Math.abs(offset.y) <= height/2)
     {
-        const _y = offset.y/zoom + height/2;
+        const _y = offset.y + height/2;
         c.beginPath();
         c.moveTo(0,_y);
         c.lineTo(width,_y);
@@ -60,37 +72,51 @@ ChangeOffset = ((event)=>{
     Draw();
 });
 
-Eval = x=>5*Math.sin(x);
-
-PlotData = (()=>{
-    
-});
-
-Draw = (()=>{
-    Clear();
-    DrawAxes();
-    Plot();
-})
-
-Plot = (()=>{
-    c.lineWidth = 1;
+PlotFunction = ((color,e)=>{
+    c.lineWidth = 2;
+    c.strokeStyle = color;
     c.beginPath();
-    
-    c.moveTo(0,0);
     
     for(let x = 0 ; x <= width ; x += precision)
     {
-        let _x = (x-offset.x)/zoom;
+        let _x = (x-offset.x-width/2)/zoom;
+        
+        let _y = e(_x);
+
+        
     
-        let _y = Eval(_x);
-    
-        let y = _y * zoom + offset.y;
-    
-        c.lineTo(x,y+height/2);
+        let y = -_y * zoom + offset.y + height/2;
+        
+        let yhf = e((x-precision/2-offset.x-width/2)/zoom);
+        let ya = e((x-precision-offset.x-width/2)/zoom);
+
+        if(ya == yhf && ya != _y )
+        {
+            c.stroke();
+            c.beginPath();
+            c.moveTo(x,y);
+        }
+        else
+        {
+            if(x==0)
+                c.moveTo(x,y);
+            c.lineTo(x,y);
+        }
     }
     
     c.stroke();
 })
 
+
+Eval = x=>10*x+5;
+
+Draw = (()=>{
+    Clear();
+    DrawAxes();
+    PlotFunction("#f0f",x=>x);
+    PlotFunction("#00f",x=>x*x);
+    PlotFunction("#070",x=>Math.sin(x));
+    PlotFunction("#a00",x=>Math.round(x));
+})
 
 Draw();
