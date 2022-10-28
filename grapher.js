775eabade1e297b87420568956639a6f3d1e75f2
Emmy43262 <73496800+Emmy43262@users.noreply.github.com>
2022-10-28 21:27:58 +0300

    Added grid with dynamic unit size and grid labels. Added point highlighting

--- grapher.js
+++ grapher.js
@@ -9,15 +9,44 @@ const height = g.height;
 const canvasData = c.getImageData(0,0,width,height);
 
 const offset = {x:0,y:0};
-let zoom = 50; //pixels per unit
+let zoom = 60; //pixels per unit
 const precision = 1; //pixel gap between evaluations
+const idealGridSize = 150; //upper bound for grid size
+const pointDisplayDelta = 30; //max pixels between mouse position and function 
+let gridSize;
+
+let hilightPoint = {x:-10,y:-10};
+
+c.font = "bold 20px monospace";
+
+const functions =[
+    {
+        color:"#f0f",
+        e:x=>Math.sqrt(x)
+    },
+    {
+        color:"#00f",
+        e:x=>x*x+2
+    },
+    {
+        color:"#070",
+        e:x=>Math.sin(x)
+    },
+    {
+        color:"#a00",
+        e:x=>Math.round(x)+x
+    },
+
+];
 
 document.getElementById("magnify").addEventListener("click",()=>{
     zoom *= 1.2;
+    ComputeGridSize();
     Draw();
 });
 document.getElementById("demagnify").addEventListener("click",()=>{
-    zoom *= .83;
+    zoom /= 1.2;
+    ComputeGridSize();
     Draw();
 });
 
@@ -33,7 +62,52 @@ g.addEventListener("mouseleave",function(event){
     g.removeEventListener("mousemove",ChangeOffset);
 })
 
-Clear = (()=>{
+g.addEventListener("mousemove",function(event){
+    cx = xToCoord(event.x);
+    cy = yToCoord(event.y);
+
+    Draw();
+    hilightPoint = {x:-10,x:-10};
+
+    let drawn = false;
+    functions.forEach(element => {
+        if(drawn)
+            return;
+        const v = yToWorld(element.e(cx));
+        if(Math.abs(v-event.y) <= pointDisplayDelta)
+        {   
+            drawn = true;
+            hilightPoint = {x:event.x,y:v};
+            
+            c.fillStyle = "#eee";
+            c.strokeStyle = "#000";
+            c.lineWidth = 1;
+            c.fillRect(hilightPoint.x,hilightPoint.y,200,50);
+            c.strokeRect(hilightPoint.x,hilightPoint.y,200,50);
+            c.fillStyle = "#000";
+            let text = "(" + (Math.round(xToCoord(hilightPoint.x)*100)/100).toString() + ", " + (Math.round(yToCoord(hilightPoint.y)*100)/100).toString() + ")";
+            c.textAlign = "center";
+            c.fillText(text,hilightPoint.x+100,hilightPoint.y+25);
+            
+            return;
+        }
+    });
+
+});
+
+toCoords = point=>{
+    point.x = (point.x-offset.x-width/2)/zoom;
+    point.y = -(point.y-offset.y-height/2)/zoom;
+    return point;
+}
+
+xToCoord = x => (x-offset.x-width/2)/zoom;
+yToCoord = y => -(y-offset.y-height/2)/zoom;
+
+xToWorld = x => x*zoom+width/2+offset.x;
+yToWorld = y => -y*zoom+height/2+offset.y;
+
+Clear = ()=>{
     c.lineWidth = 2*width;
     c.strokeStyle = '#eee';
 
@@ -41,9 +115,9 @@ Clear = (()=>{
     c.moveTo(0,0);
     c.lineTo(height,width);
     c.stroke();
-});
+};
 
-DrawAxes = (()=>{
+DrawAxes = ()=>{
     c.lineWidth = 2;
     c.strokeStyle = "#000";
 
@@ -64,7 +138,7 @@ DrawAxes = (()=>{
         c.lineTo(width,_y);
         c.stroke();
     }
-});
+};
 
 ChangeOffset = ((event)=>{
     offset.x += event.movementX;
@@ -72,7 +146,7 @@ ChangeOffset = ((event)=>{
     Draw();
 });
 
-PlotFunction = ((color,e)=>{
+PlotFunction = (color,e)=>{
     c.lineWidth = 2;
     c.strokeStyle = color;
     c.beginPath();
@@ -83,8 +157,6 @@ PlotFunction = ((color,e)=>{
         
         let _y = e(_x);
 
-        
-    
         let y = -_y * zoom + offset.y + height/2;
         
         let yhf = e((x-precision/2-offset.x-width/2)/zoom);
@@ -105,18 +177,121 @@ PlotFunction = ((color,e)=>{
     }
     
     c.stroke();
-})
+}
+
+PlotFunctions = () => {
+    functions.forEach(element => {
+        PlotFunction(element.color, element.e);
+    });
+}
+
+ComputeGridSize = () =>{
+
+    const maxSize = idealGridSize/zoom;
+    const multipliers = [1,2,4,5,1000];
+    const org = 10 ** Math.floor(Math.log10(idealGridSize/zoom));
+    
+    for(let i = 1 ; i < multipliers.length ; ++i)
+        if(org*multipliers[i] > maxSize)
+        {
+            gridSize = org*multipliers[i-1];
+            return;
+        }
+}
+ComputeGridSize();
+
+DrawGrid = () => 
+{
+    let st = (-offset.x-width/2)/zoom;
+    let en = (width/2-offset.x)/zoom;
+
+    st -= st%gridSize;
+    en -= en%gridSize;
+
+    c.strokeStyle= "#666";
+    c.lineWidth = 2;
+
+    for(let x = st ; x <= en ; x += gridSize)
+    {
+        const _x = x * zoom + width/2 + offset.x;
+        
+        c.beginPath();
+        c.moveTo(_x,0);
+        c.lineTo(_x,height);
+        c.stroke();
+    }
 
 
-Eval = x=>10*x+5;
+    st = (-offset.y-height/2)/zoom;
+    en = (height/2-offset.y)/zoom;
+
+    st -= st%gridSize;
+    en -= en%gridSize;
+
+    for(let y = st ; y <= en ; y += gridSize)
+    {
+        const _y = y * zoom + height/2 + offset.y;
+
+        c.beginPath();
+        c.moveTo(0,_y);
+        c.lineTo(width,_y);
+        c.stroke();
+    }
+}
+
+
+
+DrawLabels = () => 
+{
+    let st = (-offset.x-width/2)/zoom;
+    let en = (width/2-offset.x)/zoom;
+
+    st -= st%gridSize;
+    en -= en%gridSize;
+
+    c.textAlign = "left";
+    c.strokeStyle="#eee";
+    c.fillStyle="#000";
+    c.lineWidth = 4;
+
+    for(let x = st ; x <= en ; x += gridSize)
+    {
+        const _x = x * zoom + width/2 + offset.x;
+
+        if(x!=0)
+            if(Math.abs(offset.y) < height/2 )
+            {
+                c.strokeText(x,_x+5,offset.y+height/2+20);
+                c.fillText(x,_x+5,offset.y+height/2+20);
+            }
+        }
+
+    st = (-offset.y-height/2)/zoom;
+    en = (height/2-offset.y)/zoom;
+
+    st -= st%gridSize;
+    en -= en%gridSize;
+
+
+    for(let y = st ; y <= en ; y += gridSize)
+    {
+        const _y = y * zoom + height/2 + offset.y;
+
+        if(y!=0)
+            if(Math.abs(offset.x) < width/2)
+            {
+                c.strokeText(-y,offset.x+width/2+5,_y-6);
+                c.fillText(-y,offset.x+width/2+5,_y-6);
+            }
+    }
+}
 
 Draw = (()=>{
     Clear();
     DrawAxes();
-    PlotFunction("#f0f",x=>x);
-    PlotFunction("#00f",x=>x*x);
-    PlotFunction("#070",x=>Math.sin(x));
-    PlotFunction("#a00",x=>Math.round(x));
+    DrawGrid();
+    PlotFunctions();
+    DrawLabels();
 })
 
 Draw();
