663fe94cefdf5633b18fa599fa9c3080240fe399
Emmy43262 <73496800+Emmy43262@users.noreply.github.com>
2022-11-02 00:53:06 +0200

    added inputs and new point analyser

--- grapher.js
+++ grapher.js
@@ -1,10 +1,10 @@
 const g = document.getElementById("grapher");
 const c = g.getContext("2d");
+
 g.width = document.getElementById("wrapper").clientWidth;
 g.height = document.getElementById("wrapper").clientHeight;
-
-const width = g.width;
-const height = g.height;
+let width = g.width;
+let height = g.height;
 
 
 const offset = {x:0,y:0};
@@ -14,38 +14,18 @@ const idealGridSize = 150; //upper bound for grid size
 const pointDisplayDelta = 30; //max pixels between mouse position and function 
 let gridSize;
 
-let hilightPoint = {x:-10,y:-10};
+let hilit_function = null; 
 
+let funcs = [];
 let p10;
 let unit_v;
-
-c.font = "16px arial";
-
-const functions =[
-    {
-        color:"#f0f",
-        e:x=>Math.sqrt(x)
-    },
-    {
-        color:"#00f",
-        e:x=>x*x+2
-    },
-    {
-        color:"#070",
-        e:x=>Math.sin(x)
-    },
-    {
-        color:"#a00",
-        e:x=>Math.round(x)+x
-    },
-
-];
 
 ChangeZoom = (multiplier) =>{
     zoom *= multiplier;
     ComputeGridSize();
     Draw();
 }
+
 
 document.getElementById("magnify").addEventListener("click",()=>{
     ChangeZoom(1.2);
@@ -66,18 +46,21 @@ g.addEventListener("mouseleave",function(event){
     g.removeEventListener("mousemove",ChangeOffset);
 })
 
-g.addEventListener("mousemove",function(event){
+/*g.addEventListener("mousemove",function(event){
     cx = xToCoord(event.x);
     cy = yToCoord(event.y);
-
-    Draw();
-    hilightPoint = {x:-10,x:-10};
-
+    
     let drawn = false;
-    functions.forEach(element => {
+
+    for(let i = 0 ; i < funcs.length ; ++i)
+    {
+        let element = funcs[i];
+
         if(drawn)
-            return;
+        return;
+        
         const v = yToWorld(element.e(cx));
+        
         if(Math.abs(v-event.y) <= pointDisplayDelta)
         {   
             drawn = true;
@@ -92,27 +75,99 @@ g.addEventListener("mousemove",function(event){
             let text = "(" + (Math.round(xToCoord(hilightPoint.x)*100)/100).toString() + ", " + (Math.round(yToCoord(hilightPoint.y)*100)/100).toString() + ")";
             c.textAlign = "center";
             c.fillText(text,hilightPoint.x+100,hilightPoint.y+25);
-            
+        }
+    }
+    
+});*/
+
+g.addEventListener('click',(event)=>{
+    const {clientX,clientY} = event;
+    const sX = xToCoord(clientX);
+
+    for(let i = 0 ; i < funcs.length ; ++i)
+    {
+        if(funcs[i].fct == "")continue;
+
+        const pY = yToWorld(eval(funcs[i].fct.replace(/x/gm, sX)));
+        if(Math.abs(pY-clientY)<30)
+        {
+            hilit_function = i;
+            Draw();
             return;
         }
-    });
+    }
+    if(hilit_function != null)
+    {
+        hilit_function = null;
+        Draw();
+    }
+})
 
-});
+let drawingPointData = false;
+
+g.addEventListener('mousemove',(event)=>{
+    if(hilit_function==null)
+        return;
+    
+    const {clientX,clientY} = event;
+    const sX = xToCoord(clientX);
+    const sY = eval(funcs[hilit_function].fct.replace(/x/gm,sX));
+
+    const pY = yToWorld(sY);
+    if(Math.abs(pY-clientY)<40)
+    {
+        const x = sX.toPrecision(3);
+        const y = sY.toPrecision(3);
+        Draw();
+
+        c.fillStyle = "#fff";
+        c.fillRect(xToWorld(sX),yToWorld(sY),200,50);
+        c.strokeStyle = "#000";
+        c.lineWidth = 3;
+        c.strokeRect(xToWorld(sX),yToWorld(sY),200,50);
+
+        
+        
+        const txt = "("+x+", "+y+")";
+        
+        c.fillStyle = "#000";
+        c.font = "24px arial bold";
+        c.textAlign="center";
+        c.fillText(txt,xToWorld(sX)+100,yToWorld(sY)+30);
+
+        drawingPointData = true;
+    }
+    else if(drawingPointData)
+    {
+        Draw();
+        console.log("da");
+        drawingPointData = false;
+    }
+
+})
 
 g.addEventListener('wheel', (event) => {
-
+    
     const{wheelDeltaY, offsetX, offsetY} = event;
-
+    
     //let dX = (offsetX - width/2)/width;
     //let dY = (offsetY - height/2)/height;
-
+    
     //offset.x += dX * 100 * ((wheelDeltaY>0)?-1:1);
     //offset.y += dY * 100 * ((wheelDeltaY>0)?-1:1);
     
     const zoomValue = 117;
-
+    
     const z = (wheelDeltaY>0)?(wheelDeltaY/zoomValue):(1/Math.abs(wheelDeltaY/zoomValue));
     ChangeZoom(z);
+});
+
+window.addEventListener("resize",(event) =>{
+    g.width = document.getElementById("wrapper").clientWidth;
+    g.height = document.getElementById("wrapper").clientHeight;
+    width = g.width;
+    height = g.height;
+    Draw();
 });
 
 toCoords = point=>{
@@ -130,7 +185,7 @@ yToWorld = y => -y*zoom+height/2+offset.y;
 Clear = ()=>{
     c.lineWidth = 2*width;
     c.strokeStyle = '#fff';
-
+    
     c.beginPath();
     c.moveTo(0,0);
     c.lineTo(height,width);
@@ -149,7 +204,7 @@ DrawAxes = ()=>{
         c.lineTo(_x,height);
         c.stroke();
     }
-
+    
     if(Math.abs(offset.y) <= height/2)
     {
         const _y = offset.y + height/2;
@@ -166,43 +221,41 @@ ChangeOffset = ((event)=>{
     Draw();
 });
 
-PlotFunction = (color,e)=>{
-    c.lineWidth = 3;
-    c.strokeStyle = color;
+
+PlotFunction = (e)=>{
     c.beginPath();
     
     for(let x = 0 ; x <= width ; x += precision)
     {
         let _x = (x-offset.x-width/2)/zoom;
         
-        let _y = e(_x);
+        let _y = eval(e.fct.replace(/x/gm, _x));
 
         let y = -_y * zoom + offset.y + height/2;
         
-        let yhf = e((x-precision/2-offset.x-width/2)/zoom);
-        let ya = e((x-precision-offset.x-width/2)/zoom);
-
-        if(ya == yhf && ya != _y )
-        {
-            c.stroke();
-            c.beginPath();
+        if(x==0)
             c.moveTo(x,y);
-        }
-        else
-        {
-            if(x==0)
-                c.moveTo(x,y);
-            c.lineTo(x,y);
-        }
+        c.lineTo(x,y);
     }
     
     c.stroke();
 }
 
+colors = ["#a00","#00a","#070"];
+
 PlotFunctions = () => {
-    functions.forEach(element => {
-        PlotFunction(element.color, element.e);
-    });
+
+    for(let i = 0 ; i < funcs.length ; ++i)
+    {
+        if(i==hilit_function)
+            c.lineWidth = 4;
+        else
+            c.lineWidth = 3;
+        
+        c.strokeStyle = colors[i];
+
+        PlotFunction(funcs[i]);
+    }
 }
 
 ComputeGridSize = () =>{
@@ -298,6 +351,8 @@ DrawLabels = () =>
     st -= st%gridSize;
     en -= en%gridSize;
 
+    c.font = "16px arial";
+
     c.textAlign = "center";
     c.strokeStyle="#fff";
     c.fillStyle="#000";
@@ -340,12 +395,45 @@ DrawLabels = () =>
     }
 }
 
+
 Draw = (()=>{
     Clear();
     DrawGrid();
     DrawAxes();
     PlotFunctions();
     DrawLabels();
+    //console.log(funcs);
 })
 
+
+f_holders = document.getElementsByClassName("fun");
+
+
+for(let i = 0 ; i < f_holders.length ; ++i)
+{
+    f_holders[i].childNodes[5].addEventListener("click",()=>{
+        const str = f_holders[i].childNodes[3].value;
+
+        for(let j = 0 ; j < i ; ++j)
+        {
+            if(funcs[j] == null)
+            {
+                funcs[j] = {fct:""};
+            }
+        }
+
+        funcs[i] = {fct:""};
+        funcs[i].fct = str;
+        Draw();
+    });
+}
+
+
+
+/*f_holders.forEach(fun => {
+    console.log(fun.childNodes[2]);
+});*/
+
 Draw();
+
+
